8cfca6a26ba453b6acef3b82d9794dcba8cfbee6
dkatina <108095842+dkatina@users.noreply.github.com>
2025-10-14 19:55:48 -0400

    added models

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,16 @@
 #where I create the create_app function
+from flask import Flask
+from .models import db
+
+#create the application factory
+def create_app(config_name):
+
+    #initialze blank app
+    app = Flask(__name__)
+    #configure the app
+    app.config.from_object(f'config.{config_name}')
+
+    #Initialize extensions on app
+    db.init_app(app)
+
+    return app
--- app/models.py
+++ app/models.py
@@ -1,1 +1,47 @@
 #Where I will initialize SQLAlchemy and create my models
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Date, String, ForeignKey
+from datetime import date
+
+#Create Base Model to be inherited from
+class Base(DeclarativeBase):
+    pass
+
+#Instatiate db and set Base model
+db = SQLAlchemy(model_class=Base)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(360), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(500), nullable=False)
+    role: Mapped[str] = mapped_column(String(360), nullable=False, default='user')
+    spotify_id: Mapped[str] = mapped_column(String(360), nullable=True)
+
+    collections: Mapped[list['Collection']] = relationship('Collection', back_populates='user')
+
+
+class Collection(Base):
+    __tablename__ = 'collections'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    title: Mapped[str] = mapped_column(String(300), nullable=False)
+    release_date: Mapped[date] = mapped_column(Date, nullable=False)
+    cover_art: Mapped[str] = mapped_column(String(500), nullable=False)
+    spotify_link: Mapped[str] = mapped_column(String(500), nullable=False)
+    album_id: Mapped[str] = mapped_column(String(100), nullable=False)
+    collection_type: Mapped[str] = mapped_column(String(20), nullable=False)
+
+    user: Mapped['User'] = relationship('User', back_populates='collections')
+    
+    
+
+
+
+
+
+
